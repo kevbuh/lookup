52e2a99ab5b525ec60dea926f37a8106ce259512
Kevin Buhler <buhlerkw@gmail.com>
2023-03-16 00:22:08 -0600

    getting call

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-import { API_KEY } from './constants.js';
 
 const now = new Date(); // Get the current time
 
@@ -17,15 +16,42 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
   }
 });
 
-chrome.contextMenus.onClicked.addListener(async function(message) {
-  await generateAnswer().then(function(response) {
-    // sendResponse([response, null]);
-    console.log('generated answer!')
-  }).catch(function(error) {
-    // sendResponse([null, error]);
-    console.log('ERROR IN GENERATION')
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === "append-text") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { text: message.text });
+    });
+  }
+});
+
+
+// MAYBE USE THIS TO DOM STUFF --> sendResponse([response, null]);
+// chrome.contextMenus.onClicked.addListener(async function(message) {
+//   const res1 = await generateAnswer().then(function(response) {
+//     console.log('generated answer!')
+//   }).catch(function(error) {
+//     console.log('ERROR IN GENERATION')
+//   });
+
+//   const data = await res1;
+//   console.log('GOT THIS:::::', data)
+//   // const element = document.getElementById("selected-text"); // Get the element by ID
+//   // element.textContent += data; // Append text to the element's content
+
+//   return true;
+// });
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  await generateAnswer().then(async (res)=> {
+    console.log("GOT THIS-->",res.api_response);
+    // document.getElementById("look-up-clicked").textContent += res;
+    // chrome.tabs.sendMessage(tab.id, { action: "append-text", text: res.api_response });
+    // inject content script and send message
+    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    console.log('before message call');
+    const response = await chrome.runtime.sendMessage(tab.id, { data: res.api_response }).then((res) =>{console.log('yuh', res);})
+    console.log('ENDD::::');
   });
-  return true;
 });
 
 async function generateAnswer() {
@@ -35,7 +61,7 @@ async function generateAnswer() {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${API_KEY}`,
+      Authorization: `Bearer `,
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
@@ -47,6 +73,7 @@ async function generateAnswer() {
   const data = await response.json();
   // console.log('Response data:', data);
   console.log('response:', data.choices[0].message.content);
+  const api_response = data.choices[0].message.content;
 
 
   
@@ -85,7 +112,7 @@ async function generateAnswer() {
 
   console.log('end of generation function!')
   
-  return {};
+  return {"api_response":api_response};
 }
 
 
